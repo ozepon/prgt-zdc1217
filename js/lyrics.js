5d9cd83a199c0cb40e8dc25bf3926e876a594d61
ozepon <dandy.buzz@gmail.com>
2017-12-10 19:15:55 +0900

    add font size change function.

--- js/lyrics.js
+++ js/lyrics.js
@@ -5,35 +5,60 @@ window.onload = function() {
     for(var i = 0; i < callList.length; i++) {
       callList[i].style.display = 'none';
     }
+  }
+  
 
-    // show anotetion
-    document.getElementsByClassName('comming_soon')[0].style.display = 'block';
-
-    // font size を変更する
-    var p = document.getElementsByTagName("pre");
-    window.app.preLyric = p[0];
-    var defaultFontSize = 13;
-    var maxFontSize = 20;
-    var minFontSize = 8;
-    console.log(window.app.preLyric.style.fontSize);
-
-    // font size up
-    window.app.fontSizeUp = function() {
-      // 未設定の場合は初期化
-      if(window.app.preLyric.style.fontSize) {
-        window.app.preLyric.style.fontSize = defaultFontSize;
-        console.log(window.app.preLyric.style.fontSize);
-        return;
-      } else if (window.app.preLyric.style.fontSize < maxFontSize){
-        // fontSizeup
-        window.app.preLyric.style.fontSize = window.app.preLyric.style.fontSize + 1;
-        console.log(window.app.preLyric.style.fontSize);
-      }
-    }
-
-    // font size down
-    window.app.fontSizeDown = function() {
-      console.log('down');
+  // show anotetion
+  document.getElementsByClassName('comming_soon')[0].style.display = 'block';
+  
+  // font size 変更周り　--------
+  var preLyric;
+  var defaultFontSize = 0.6;
+  var maxFontSize = 1.5;
+  var minFontSize = 0.4;
+ 
+  // font size up
+  function fontSizeUp() {
+    var sizeNum = parseFloat(preLyric.style.fontSize.replace(/px|rem|em/,''));
+    
+    // 未設定の場合は初期化
+    if(!sizeNum) {
+      preLyric.style.fontSize = (defaultFontSize + 0.1) + 'rem';
+      console.log(preLyric.style.fontSize);
+    } else if (sizeNum < maxFontSize){
+      // fontSizeup
+      preLyric.style.fontSize = (sizeNum + 0.1) + 'rem';
+      console.log(preLyric.style.fontSize);
     }
   }
+ 
+  // font size down
+  function fontSizeDown() {
+    var sizeNum = parseFloat(preLyric.style.fontSize.replace(/px|rem|em/,''));
+    
+    // 未設定の場合は初期化
+    if(!sizeNum) {
+      preLyric.style.fontSize = (defaultFontSize - 0.1) + 'rem';
+      console.log(preLyric.style.fontSize);
+    } else if (minFontSize < sizeNum){
+      // fontSizeup
+      preLyric.style.fontSize = (sizeNum - 0.1) + 'rem';
+      console.log(preLyric.style.fontSize);
+    }
+  }
+  //lyricを表示するpre
+  var p = document.getElementsByTagName("pre");
+  preLyric = p[0];
+
+  // functionをbind
+  var fontSizeUpButton = document.getElementById('fontSizeUp');
+  fontSizeUpButton.addEventListener('click', function() {
+    fontSizeUp();
+  });
+  var fontSizeDownButton = document.getElementById('fontSizeDown');
+  fontSizeDownButton.addEventListener('click', function() {
+    fontSizeDown();
+  });
+
+
 }
